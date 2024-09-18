b2e317368d1761683ce0227889404267e4ea0ffb
Lycoris <zzyo.yj@outlook.com>
2024-09-18 15:02:48 +0800

    1

--- src/Lycoris.CSRedisCore.Extensions/Services/IRedisHashCache.cs
+++ src/Lycoris.CSRedisCore.Extensions/Services/IRedisHashCache.cs
@@ -177,6 +177,13 @@ namespace Lycoris.CSRedisCore.Extensions.Services
 		/// </summary>
 		/// <param name="key"></param>
 		/// <param name="value"></param>
+		bool Set<T>(string key, params (string, T)[] value) where T : class;
+
+		/// <summary>
+		/// 同时将多个 field-value (域-值)对设置到哈希表 key 中
+		/// </summary>
+		/// <param name="key"></param>
+		/// <param name="value"></param>
 		/// <returns></returns>
 		Task<bool> SetAsync(string key, params (string, string)[] value);
 
@@ -188,12 +195,12 @@ namespace Lycoris.CSRedisCore.Extensions.Services
 		Task<bool> SetAsync<T>(string key, params (string, T)[] value) where T : class;
 
 		/// <summary>
-		/// 
+		/// 将哈希表 key 中的字段 field 的值设为 value 。
 		/// </summary>
 		/// <param name="key"></param>
 		/// <param name="fieId"></param>
 		/// <param name="value"></param>
-		/// <returns></returns>
+		/// <returns>如果字段是哈希表中的一个新建字段,并且值设置成功,返回true。如果哈希表中域字段已经存在且旧值已被新值覆盖,返回false。</returns>
 		bool Set(string key, string fieId, string value);
 
 		/// <summary>
@@ -205,6 +212,15 @@ namespace Lycoris.CSRedisCore.Extensions.Services
 		/// <param name="value"></param>
 		/// <returns>如果字段是哈希表中的一个新建字段,并且值设置成功,返回true。如果哈希表中域字段已经存在且旧值已被新值覆盖,返回false。</returns>
 		bool Set<T>(string key, string fieId, T value) where T : class;
+
+		/// <summary>
+		/// 将哈希表 key 中的字段 field 的值设为 value 。
+		/// </summary>
+		/// <param name="key"></param>
+		/// <param name="fieId"></param>
+		/// <param name="value"></param>
+		/// <returns>如果字段是哈希表中的一个新建字段,并且值设置成功,返回true。如果哈希表中域字段已经存在且旧值已被新值覆盖,返回false。</returns>
+		Task<bool> SetAsync(string key, string fieId, string value);
 
 		/// <summary>
 		/// 将哈希表 key 中的字段 field 的值设为 value 。
